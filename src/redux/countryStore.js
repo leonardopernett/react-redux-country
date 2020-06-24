8d4f254ed3068dd3128336c0aae028ab1ab6935b
leonardo pernett <pernettleonardo@gmail.com>
2020-06-24 13:37:37 -0500

    stable project

--- src/redux/countryStore.js
+++ src/redux/countryStore.js
@@ -1,29 +1,31 @@
 const initialization = {
   countryList: [],
-  countryByName:[],
-  countryByRegion:[]
+  countryByName: [],
+  countryByRegion: [],
 };
 
 export const createReducer = (state = initialization, actions) => {
-    if(actions.type==="GET_LIST_COUNTRY"){
-        return {
-            ...state,
-            countryList:actions.payload
-        }
-    }
-    if(actions.type==="GET_LIST_BY_NAME"){
-       return {
-           ...state, 
-           countryByName:state.countryList.filter(country => country.name.toLowerCase().includes(actions.payload.toLowerCase()))
-         }
-        
-     }
-     if(actions.type==="GET_FILTER_REGION"){
-         return {
-             ...state,
-             countryByRegion:state.countryList.filter(country=>country.region.toLowerCase()===actions.payload.toLowerCase())
-         }
-     }
-    return state;
+  if (actions.type === "GET_LIST_COUNTRY") {
+    return {
+      ...state,
+      countryList: actions.payload,
+    };
+  }
+  if (actions.type === "GET_LIST_BY_NAME") {
+    return {
+      ...state,
+      countryByName: state.countryList.filter((country) =>
+        country.name.toLowerCase().includes(actions.payload.toLowerCase())
+      ),
+    };
+  }
+  if (actions.type === "GET_FILTER_REGION") {
+    return {
+      ...state,
+      countryByRegion: state.countryList.filter((country) => 
+         country.region.toLowerCase() === actions.payload.toLowerCase()
+      ),
+    };
+  }
+  return state;
 };
-
